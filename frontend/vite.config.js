0ae4a16dffac49c165ab1eeddbbf4337ab902210
Rivero Peña Jose Pedro <jprivero4@gmail.com>
2025-06-28 22:37:30 +0000

    fix imprts prueba 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   "compilerOptions": {
-    "jsx": "react-jsx"
+  esbuild: {
+    jsx: 'automatic' // 👈 CLAVE
   }
-})
+});
